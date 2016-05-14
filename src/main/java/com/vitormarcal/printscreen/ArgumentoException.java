31d6d789f19c9f2d2e3c2eb72b4fd50e814d53c8
Vítor Marçal <git@vitormarcal.com.br>
2016-05-14 20:06:19 -0300

    Update versao 1

---
+++ src/main/java/com/vitormarcal/printscreen/ArgumentoException.java
@@ -0,0 +1,13 @@
+package com.vitormarcal.printscreen;
+
+public class ArgumentoException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public ArgumentoException(String msg) {
+		super(msg);
+	}
+
+
+
+}
