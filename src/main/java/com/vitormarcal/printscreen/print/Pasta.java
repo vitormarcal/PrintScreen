842e5badfadf8976173be9e45cda8d7ba3adfdae
vitormarcal <vitormarcal8@gmail.com>
2018-12-03 21:22:57 -0200

    REFACTOR issues warnings

--- src/main/java/com/vitormarcal/printscreen/print/Pasta.java
+++ src/main/java/com/vitormarcal/printscreen/print/Pasta.java
@@ -1,46 +1,52 @@
 package com.vitormarcal.printscreen.print;
 
+import lombok.extern.log4j.Log4j2;
+
 import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@Log4j2
 public class Pasta {
-	private File arq;
-	private String path;
-	private String pathPersonalizado = "";
-	private Date data;
+    private File arq;
+    private String path;
+    private String pathPersonalizado = "";
 
-	public Pasta() {
-		criaPastaRaiz();
-	}
+    public Pasta() {
+        criaPastaRaiz();
+    }
 
-	public void criaPastaRaiz() {
-		this.path = pathPersonalizado + "\\PrintScreen\\";
-		this.arq = new File(path);
-		this.arq.mkdirs();
-	}
+    private void criaPastaRaiz() {
+        this.path = pathPersonalizado + "\\PrintScreen\\";
+        this.arq = new File(path);
+        if (this.arq.mkdirs()) {
+            log.info("Criado pasta {} ", arq.toString());
+        }
+    }
 
-	public void criaSubDiretorio() {
-		this.path = pathPersonalizado + "\\PrintScreen\\" + fmtDataPasta();
-		this.arq = new File(path);
-		this.arq.mkdirs();
-	}
+    public void criaSubDiretorio() {
+        this.path = pathPersonalizado + "\\PrintScreen\\" + fmtDataPasta();
+        this.arq = new File(path);
+        if (this.arq.mkdirs()) {
+            log.info("Criado subdiretorio {} ", arq.toString());
+        }
+    }
 
-	String fmtDataPasta() {
-		DateFormat df = new SimpleDateFormat("yyy\\MM\\dd");
-		this.data = new Date();
-		return df.format(this.data);
-	}
+    private String fmtDataPasta() {
+        DateFormat df = new SimpleDateFormat("yyy\\MM\\dd");
+        Date data = new Date();
+        return df.format(data);
+    }
 
-	public String getPath() {
-		return this.path;
-	}
+    String getPath() {
+        return this.path;
+    }
 
-	public void setPathPersonalizado(String pathPersonalizado) {
-		if (pathPersonalizado != null) {
-			this.pathPersonalizado = pathPersonalizado;
-		}
+    public void setPathPersonalizado(String pathPersonalizado) {
+        if (pathPersonalizado != null) {
+            this.pathPersonalizado = pathPersonalizado;
+        }
 
-	}
+    }
 }
